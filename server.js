ffcd53ec1e207b15aeecf6d9b59c9b9a01e83d09
Trish <trishbandhekar97@outlook.com>
2023-09-17 21:05:29 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const app = express();
+const fs = require("fs");
 const port = 3000;
 
 const budget = {
@@ -44,7 +45,21 @@ app.get('/hello', (req,res) => {
 });
 
 app.get('/budget', (req,res)=>{
-    res.json(budget);
+    fs.readFile("data.json", "utf-8", (err,jsonData) => {
+
+        if (err) {
+            console.log("Error reading file:", err);
+            return;
+        }
+
+        try {
+            const budgetData = JSON.parse(jsonData);
+            res.json(budgetData);
+
+        } catch (err) {
+            console.log("Error parsing JSON string:", err);
+        }
+    });
 })
 
 app.listen(port, ()=> {
