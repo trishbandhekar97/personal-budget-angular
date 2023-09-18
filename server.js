79015639ac2456b03aaff92b07db4b55e4d06056
Trish <trishbandhekar97@outlook.com>
2023-09-17 22:47:37 -0400

    Added D3JS chart

--- server.js
+++ server.js
@@ -4,38 +4,6 @@ const app = express();
 const fs = require("fs");
 const port = 3000;
 
-const budget = {
-    myBudget: [
-    {
-        title: 'Rent',
-        budget: 515
-    },
-    {
-        title: 'Movies',
-        budget: 50
-    },
-    {
-        title: 'Groceries',
-        budget: 150
-    },
-    {
-        title: 'Netflix',
-        budget: 15
-    },
-    {
-        title: 'Shopping',
-        budget: 100
-    },
-    {
-        title: 'Restaurant',
-        budget: 65
-    },
-    {
-        title: 'Fuel',
-        budget: 120
-    },
-]}
-
 
 app.use('/', express.static('public'));
 
